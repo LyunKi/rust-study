b746373025f93d50b8e0dabf99d4a65ecf755406
龙云 <longyun@co-mall.com>
2018-12-29 17:59:36 +0800

    learning module system in rust

---
+++ src/enum.rs
@@ -0,0 +1,28 @@
+#[derive(Debug)]
+enum UsState {
+    A(String),
+    B(String),
+}
+
+enum Coin {
+    C,
+    D(UsState),
+    E,
+}
+
+fn main() {
+    let a = Coin::D(UsState::B(String::from("b")));
+    match a {
+        Coin::C => println!("coin c"),
+        Coin::E => println!("coin e"),
+        Coin::D(d) => println!("{:#?}", d)
+    };
+
+    if let Coin::C = a {
+        println!("coin c")
+    } else if let Coin::D(e) = a{
+        println!("coin d,{:#?}", e);
+    } else {
+        println!("coin other")
+    }
+}
---
+++ src/struct.rs
@@ -0,0 +1,56 @@
+struct User {
+    name: String,
+    email: String,
+    active: bool,
+    sign_in_count: u64,
+}
+
+fn build_user(name: String, email: String) -> User {
+    User {
+        email,
+        name,
+        active: true,
+        sign_in_count: 1,
+    }
+}
+
+struct Point(i32,i32);
+
+#[derive(Debug)]
+struct Rectangle{width:u32,height:u32}
+
+impl Rectangle{
+    fn area(&self)->u32{
+        self.width*self.height
+    }
+
+    fn can_hold(&self,other:&Rectangle)->bool{
+        self.height >= other.height && self.width >= other.width
+    }
+
+    fn square(size:u32)->Rectangle{
+        Rectangle { width: size, height: size }
+    }
+}
+
+fn main() {
+    let mut name = String::from("a");
+    let user1 = build_user(name, String::from("b"));
+//    name.push_str("2"); 因为name的所有权被转移到user1中了，除非name拥有实现了Copy trait的类型
+    println!("{}", user1.name);
+    let mut user2 = User {
+        active: false,
+        ..user1
+    };
+    println!("{}", user2.active);
+    user2.active = true;
+    println!("{}", user2.active);
+
+    let point1 = Point(0, 0);
+    println!("{}", point1.0);
+
+    let rectangle1 = Rectangle { width: 20, height: 10 };
+    println!("the area of rectangle {:#?} is {}", rectangle1, rectangle1.area());
+    let rectangle2 = Rectangle::square(10);
+    println!("rectangle1 can holed rectangle2? the result is {}",rectangle1.can_hold(&rectangle2));
+}
